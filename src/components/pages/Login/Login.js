ca75771db22a3bc706d62006f876698b9f6cea27
Jordan <jordan@globaldataconsortium.com>
2019-07-12 09:49:09 -0400

    auth

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -2,18 +2,10 @@ import React from "react";
 import { useAuth0 } from "../../../utils/Auth0/Auth0Wrapper";
 
 const Login = () => {
-  const { loginWithRedirect, loading } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
   return (
     <div>
-      <button
-        onClick={() => {
-          if (loading === false) {
-            loginWithRedirect({});
-          }
-        }}
-      >
-        Login
-      </button>
+      <button onClick={() => loginWithRedirect({})}>Login</button>
     </div>
   );
 };
